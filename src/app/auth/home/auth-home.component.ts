16f61edaebe4ed7075a29f7ab673d603ee81a0fb
Alex Arriaga <alex.arriaga@base22.com>
2018-05-28 18:50:09 -0500

    Crear componente para home autenticado

---
+++ src/app/auth/home/auth-home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-home',
+  templateUrl: './auth-home.component.html',
+  styleUrls: ['./auth-home.component.scss']
+})
+export class AuthHomeComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
