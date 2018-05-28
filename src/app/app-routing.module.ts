467b3d75858df8dc5c8f39101bb9507f92c78ba3
Alex Arriaga <alex.arriaga@base22.com>
2018-05-28 18:51:30 -0500

    Agregar ruta para autenticación (home)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,26 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { VehicleDetailComponent } from './public/vehicles/detail/vehicle-detail.component';
 import { HomeComponent } from './public/home/home.component';
+import { AuthGuard } from './common/guards/auth.guard';
+import { PublicGuard } from './common/guards/public.guard';
+import { AuthHomeComponent } from './auth/home/auth-home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {
+    path: '', component: HomeComponent
+  },
+  // {
+  //   path: 'login',
+  //   component: LoginComponent,
+  //   pathMatch: 'full',
+  //   canActivate: [PublicGuard]
+  // },
+  {
+    path: 'auth-home',
+    component: AuthHomeComponent,
+    data: {name: 'AuthHome'},
+    canActivate: [AuthGuard]
+  },
   {path: 'vehicles/:id', component: VehicleDetailComponent}
 ];
 
